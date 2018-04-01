3d883fbe7a375d22b2108ee911b93a71b37fd6fa
Levent Dane <limon@verj.us>
2018-03-31 20:18:50 -0500

    Some stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 
 #include <stdlib.h>
 #include <string>
+#include <algorithm>
 #include <iostream>
+#include <sstream>
 
 // include the sql parser
 #include "SQLParser.h"
@@ -18,10 +20,27 @@
 
 using hsql::kStmtSelect;
 using hsql::kStmtInsert;
-using hsql::kStmtCreate;
 using hsql::kStmtDelete;
 using hsql::kStmtDrop;
 using hsql::kTableSelect;
+using hsql::ColumnDefinition;
+
+
+bool icompare_pred(unsigned char a, unsigned char b)
+{
+    return std::tolower(a) == std::tolower(b);
+}
+
+bool icompare(std::string const& a, std::string const& b)
+{
+    if (a.length()==b.length()) {
+        return std::equal(b.begin(), b.end(),
+                           a.begin(), icompare_pred);
+    }
+    else {
+        return false;
+    }
+}
 
 
 void log(std::string logbuf) {
@@ -30,10 +49,6 @@ void log(std::string logbuf) {
 
 void selectData(const hsql::SelectStatement* stmt) {
 	printf("select\n");
-	std::FILE* f = openTable(stmt->fromTable, "rb");
-	//read catalog
-	fclose(f);
-	delete f;
 }
 
 void insertData(const hsql::InsertStatement* stmt) {
@@ -46,12 +61,18 @@ void deleteData(const hsql::DeleteStatement* stmt) {
 	//read catalog
 }
 
-void createTable(const hsql::CreateStatement* stmt) {
-	printf("create %s\n", stmt->tableName);
-	std::FILE* f = openTable(stmt->tableName, "wb");
-	//update catalog
-	fclose(f);
-	delete f;
+void createTable(const std::string query) {
+	std::string tableName, fields;
+	std::string nQuery = query.substr(6);
+	std::cout << nQuery;
+	std::stringstream ss(nQuery);
+	if(std::getline(ss, tableName, '(')) {
+		std::cout << "Ups";
+	}
+	if(std::getline(ss, fields, ')')) {
+		std::cout << "Ups";
+	}
+	std::cout << tableName << " " << fields;
 }
 void dropTable(const hsql::DropStatement* stmt) {
 	//update catalog
@@ -70,9 +91,6 @@ void dispatchStatement(const hsql::SQLStatement* stmt) {
 		case kStmtDelete:
 		    deleteData((const hsql::DeleteStatement*) stmt);
 			break;
-		case kStmtCreate:
-		    createTable((const hsql::CreateStatement*) stmt);
-			break;
 		case kStmtDrop:
 			dropTable((const hsql::DropStatement*) stmt);
 			break;
@@ -87,6 +105,40 @@ hsql::SQLParserResult* parse(std::string query){
 	return result;
 }
 
+void parseCommand(std::string myStatement) {
+	if (myStatement == "quit"){
+		return 0;
+	} else if (icompare(myStatement.substr(0,6),"create")) {
+		std::cout << "Create";
+		createTable(myStatement);
+	} else if (icompare(myStatement.substr(0,10),"show table")) {
+		std::cout << "Show";
+	} else {
+		hsql::SQLParserResult* result = hsql::SQLParser::parseSQLString(myStatement);
+		if (result->isValid()) {
+			printf("Parsed successfully!\n");
+			printf("Number of statements: %lu\n", result->size());
+
+			for (uint i = 0; i < result->size(); ++i) {
+				;const hsql::SQLStatement* statement = result->getStatement(i);
+
+				dispatchStatement(statement);
+
+				hsql::printStatementInfo(statement);
+			}
+
+			delete result;
+		} else {
+			fprintf(stderr, "Given string is not a valid SQL query.\n");
+			fprintf(stderr, "%s (L%d:%d)\n", 
+					result->errorMsg(),
+					result->errorLine(),
+					result->errorColumn());
+			delete result;
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
         fprintf(stderr, "Usage: ./example \"SELECT * FROM test;\"\n");
@@ -96,32 +148,7 @@ int main(int argc, char *argv[]) {
 			printf("\nSQL> ");
 			std::string myStatement;
 			getline(std::cin, myStatement);
-			if (myStatement == "quit"){
-				return 0;
-			} else {
-				hsql::SQLParserResult* result = hsql::SQLParser::parseSQLString(myStatement);
-				if (result->isValid()) {
-					printf("Parsed successfully!\n");
-					printf("Number of statements: %lu\n", result->size());
-
-					for (uint i = 0; i < result->size(); ++i) {
-						const hsql::SQLStatement* statement = result->getStatement(i);
-
-						dispatchStatement(statement);
-
-						hsql::printStatementInfo(statement);
-					}
-
-					delete result;
-				} else {
-					fprintf(stderr, "Given string is not a valid SQL query.\n");
-					fprintf(stderr, "%s (L%d:%d)\n", 
-							result->errorMsg(),
-							result->errorLine(),
-							result->errorColumn());
-					delete result;
-				}
-			}
+			parseCommand(myStatement);
 		}
     }
 	return 0;
