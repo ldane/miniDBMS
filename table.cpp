b5fb33f55b774b218b52b624090cb7e3d627d30e
victorlcuh <31748109+victorlcuh@users.noreply.github.com>
2018-04-01 18:29:05 -0500

    changed to size_t

--- table.cpp
+++ table.cpp
@@ -243,7 +243,7 @@ int Table::getColumnBytePosition(std::string clmn){
 }
 
 int Table::getColumnByteSize(std::string clmn){
-	int pos = 0;
+	std::size_t pos = 0;
 	for (auto const& value : columnNames){
 		if (value == clmn){
 			std::string focus = getColumnType(value);
