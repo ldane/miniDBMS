3287d3e9f6e5f09c10cc2c7cf7e44f5dd1431b62
victorlcuh <31748109+victorlcuh@users.noreply.github.com>
2018-03-31 20:59:22 -0500

    test

--- catalog.cpp
+++ catalog.cpp
@@ -106,11 +106,13 @@ void Catalog::writeToFile(std::string fileName){
 	// totalsize=65
 	// records=5
 	std::ofstream ofs(fileName);
+	std::cout << "testing if made it here line 109\n";
 	if (ofs.is_open()){
 		// for each table in the map 
 		// print the schema of the table if the temporary and dropped flags are both false
 		for (const auto& kv : tables){
 			if (!kv.second->isTemporary() && !kv.second->isDropped()){
+				std::cout << "test2\n";
 				ofs << kv.second->getFormattedMetaData();
 			}
 		}
