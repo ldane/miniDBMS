63e8596d3f180c91573a70032a8625770b48daf7
victorlcuh <31748109+victorlcuh@users.noreply.github.com>
2018-04-01 17:16:41 -0500

    changed print statement of parserecords

--- table.cpp
+++ table.cpp
@@ -195,16 +195,19 @@ std::string Table::parseRecord(char* buffer) {
 	for (auto& c: columnTypesMap) {
 		if(c.second == "INT") {
 			i = (int) *buffer;
-			ss << c.first << ":" << i << "\n";
+			//ss << c.first << ":" << i << "\n";
+			ss << i;
 			buffer+=sizeof(int);
 		} else if (c.second.substr(0,4) == "CHAR") {
 			size_t pos = c.second.length();
 			int v = atoi(c.second.substr(5,pos-5).c_str());
 			s = new char[v];
 			std::strncpy(s,buffer,v);
-			ss << c.first << ":" << s << "\n";
+			//ss << c.first << ":" << s << "\n";
+			ss << s;
 			buffer+=v;
 		}
+		ss << " ";
 	}
 	return ss.str();
 }
