0ede2e74c2f55362f05f8fc2e90a7eb76a397e2f
victorlcuh <31748109+victorlcuh@users.noreply.github.com>
2018-04-30 00:29:10 -0500

    a

--- main.cpp
+++ main.cpp
@@ -488,8 +488,8 @@ void parseCommand(std::string myStatement) {
 		if (icompare(myStatement.substr(colpos+4, length), myStatement.substr(equalpos+1, length))){
 			std::cout << "This is an update with incrementing/decrementing function\n";
 			//add the ' ' around the thing.
-			myStatement.insert(equalpos+1+length+1), '\'');
-			myStatement.insert(equalpos+1 ,'\'');
+			myStatement.insert(equalpos+1+length+1), "'");
+			myStatement.insert(equalpos+1 ,"'");
 			std::cout << myStatement << "\n";
 			return;
 		}
