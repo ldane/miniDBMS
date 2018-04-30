ea490792791c0667e1a16fee1a0e8183ccfc4ddc
victorlcuh <31748109+victorlcuh@users.noreply.github.com>
2018-04-30 15:35:23 -0500

    a

---
+++ scriptbroken.sql
@@ -0,0 +1,1101 @@
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=7;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=7;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=7;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=10;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=7;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=7;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=40;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=10;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=3;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=3;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=7;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=5;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=10;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=9;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=6;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=9;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=8;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=5;
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=11;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=4;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance-1
+WHERE accountid=10;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+BEGIN TRANSACTION
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=11;
+
+UPDATE account
+SET balance=balance+1
+WHERE accountid=2;
+
+COMMIT;
+END TRANSACTION;
+
+SELECT * FROM account;
