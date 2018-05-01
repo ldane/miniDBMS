d10952a2d461ca333479c08f0d23ebd523021933
Levent Dane <limon@verj.us>
2018-04-30 19:16:09 -0500

    Add string pk

---
+++ createstr.sql
@@ -0,0 +1,14 @@
+CREATE TABLE account(personid INT, accountid CHAR(5), balance INT, PRIMARY KEY(accountid));
+
+INSERT INTO account VALUES(1,'1',10);
+INSERT INTO account VALUES(1,'2',20);
+INSERT INTO account VALUES(1,'3',15);
+INSERT INTO account VALUES(2,'4',19);
+INSERT INTO account VALUES(2,'5',15);
+INSERT INTO account VALUES(3,'6',5);
+INSERT INTO account VALUES(3,'7',19);
+INSERT INTO account VALUES(3,'8',23);
+INSERT INTO account VALUES(3,'9',96);
+INSERT INTO account VALUES(4,'10',15);
+
+
